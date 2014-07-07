8531ee093331e1e91b53caab93ce29b9318a455b
Liam H <hamblinliam@yahoo.com>
2014-07-07 17:08:28 +0100

    Used a tool to make the scripts more readable

--- main.js
+++ main.js
@@ -1,63 +1,63 @@
 var canvas, ctx, level1, tiled_data, resourcesLoaded = false;
 
 
-function main(){
+function main() {
 
-	canvas  = document.createElement("canvas");
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-	document.body.appendChild(canvas);
+    canvas = document.createElement("canvas");
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    document.body.appendChild(canvas);
 
-	ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
 
-	init();
+    init();
 
 
 }
 
-function init(){
-	tiled_data = new tiled({
-		filename: "map.json"
-	},function(layer){
-			level1 = new map({
-				width: layer.width * layer.tilewidth,
-				height: layer.height * layer.tileheight,
-				tileWidth: layer.tilewidth,
-				tileHeight: layer.tileheight
-			});
-		runner();
-	});
-	
-	
-}
+function init() {
+    tiled_data = new tiled({
+        filename: "map.json"
+    }, function (layer) {
+        level1 = new map({
+            width: layer.width * layer.tilewidth,
+            height: layer.height * layer.tileheight,
+            tileWidth: layer.tilewidth,
+            tileHeight: layer.tileheight
+        });
+        runner();
+    });
 
-function runner(){
-	var loop = function(){
-			render();
-			update();
-			draw();
-		window.requestAnimationFrame(loop, ctx);
-	};
-	if(resourcesLoaded){
-	window.requestAnimationFrame(loop, ctx);
-	}else{
-		setTimeout(function(){
-			runner();
-		}, 1);
-	}	
 
 }
 
-function render(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	tiled_data.render(ctx, level1.hashMap);
-		
+function runner() {
+    var loop = function () {
+        render();
+        update();
+        draw();
+        window.requestAnimationFrame(loop, ctx);
+    };
+    if (resourcesLoaded) {
+        window.requestAnimationFrame(loop, ctx);
+    } else {
+        setTimeout(function () {
+            runner();
+        }, 1);
+    }
+
+}
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    tiled_data.render(ctx, level1.hashMap);
+
 }
 
 
 
-function update(){}
+function update() {}
 
-function draw(){}
+function draw() {}
 
 main();
--- map.js
+++ map.js
@@ -1,26 +1,26 @@
-function map(settings){
-	this.hashMap = [];
-	this.width = settings.width;
-	this.height = settings.height;
-	this.tileWidth = settings.tileWidth;
-	this.tileHeight = settings.tileHeight;
+function map(settings) {
+    this.hashMap = [];
+    this.width = settings.width;
+    this.height = settings.height;
+    this.tileWidth = settings.tileWidth;
+    this.tileHeight = settings.tileHeight;
 
-	this.rows = this.width / this.tileWidth;
-	this.columns = this.height / this.tileHeight;
+    this.rows = this.width / this.tileWidth;
+    this.columns = this.height / this.tileHeight;
 
-	this.init = function(){
-		for(r = 0; r < this.rows; r++){
-			for(c = 0; c < this.columns; c++){
-				this.hashMap.push({
-					x: c*this.tileWidth,
-					y: r*this.tileHeight,
-					width: this.tileWidth,
-					height: this.tileHeight
-				});
-			}
-		}
-	};
+    this.init = function () {
+        for (r = 0; r < this.rows; r++) {
+            for (c = 0; c < this.columns; c++) {
+                this.hashMap.push({
+                    x: c * this.tileWidth,
+                    y: r * this.tileHeight,
+                    width: this.tileWidth,
+                    height: this.tileHeight
+                });
+            }
+        }
+    };
 
-	this.init();
+    this.init();
 
 }
